617faa8e09a1f7e68b6dba6f0b18b0e00ec391a8
akitisa-epic <36486155+akitisa-epic@users.noreply.github.com>
2018-04-17 16:46:49 -0400

    Unreal Qt/PySide2 framework

---
+++ framework.py
@@ -0,0 +1,44 @@
+"""
+Framework containing PySide2 distributions for the Unreal engine
+
+Because Unreal does not include PySide2/Qt distributions but does use its own
+version of Python, we have to distribute full versions for the engine to function.
+"""
+
+import sgtk
+import sys
+import os
+
+class UnrealQtFramework(sgtk.platform.Framework):
+
+    ##########################################################################################
+    # init and destroy
+            
+    def init_framework(self):
+        self.log_debug("%s: Initializing UnrealQtFramework..." % self)
+        
+        # Supporting Windows only for now
+        pyside_root = None            
+        if sys.platform == "win32":
+            pyside_root = os.path.join(self.disk_location, "resources", "pyside2-5.9.0a1")
+
+        if pyside_root:
+            # Add PySide2 path to PYTHONPATH
+            if pyside_root not in sys.path:
+                sys.path.append(pyside_root)
+
+            try:
+                self.log_debug("Attempting to import PySide2 from %s" % pyside_root)
+                from PySide2 import QtCore, QtGui, QtWidgets
+                import PySide2
+                
+                self.log_debug("Successfully initialized PySide2 '%s' located in %s." 
+                               % (PySide2.__version__, PySide2.__file__))
+            except ImportError:
+                pass
+            except Exception, e:
+                self.log_warning("Error setting up PySide2. Pyside2-based UI support will not "
+                                 "be available: %s" % e)        
+
+    def destroy_framework(self):
+        self.log_debug("%s: Destroying UnrealQtFramework..." % self)
