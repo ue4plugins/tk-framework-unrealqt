1301620fe70a0d53eec46f11dfa1dce9d142d829
Stéphane Deverly <39291844+gplsteph@users.noreply.github.com>
2021-09-29 10:34:36 +0200

    6644 osx support (#1)
    Revisited PySide2 shipping and added build script.
    Windows build is still included for the time being.

--- framework.py
+++ framework.py
@@ -12,6 +12,10 @@ version of Python, we have to distribute full versions for the engine to functio
 import sgtk
 import sys
 import os
+import platform
+import site
+import re
+
 
 class UnrealQtFramework(sgtk.platform.Framework):
 
@@ -21,31 +25,105 @@ class UnrealQtFramework(sgtk.platform.Framework):
     def init_framework(self):
         self.log_debug("%s: Initializing UnrealQtFramework..." % self)
         
-        # Supporting Windows only for now
-        pyside_root = None            
-        if sys.platform == "win32":
-            if sys.version_info[0] >= 3:
-                pyside_root = os.path.join(self.disk_location, "resources", "pyside2-5.15.2")
+        # This framework ships with additional Python packages
+        # make these packages available to apps and engines using
+        # this framework
+
+        # Remap the platform name to our names
+        pname = self.platform_name()
+
+        # Virtual env has different structures on Windows
+        if pname == "windows":
+            bin_folder = "Scripts"
+        else:
+            bin_folder = "bin"
+
+        # Copied over from activate_this.py script which does not exist anymore
+        # from Python 3.
+        python_major = sys.version_info[0]  # 2 or 3
+
+        base_path = os.path.realpath(
+            os.path.join(
+                os.path.dirname(__file__),
+                "python",
+                "vendors",
+                "py%d" % python_major,
+                pname,
+            )
+        )
+        self.logger.debug("Activating custom packages with %s" % base_path)
+
+        if pname == "windows":
+            site_path = os.path.join(base_path, "Lib", "site-packages")
+        else:
+            lib_folders = os.listdir(
+                os.path.join(
+                    base_path,
+                    "lib"
+                )
+            )
+            python_pattern = r"^python%d\.\d$" % python_major
+            for folder in lib_folders:
+                if re.match(python_pattern, folder):
+                    break
             else:
-                pyside_root = os.path.join(self.disk_location, "resources", "pyside2-5.9.0a1")
+                raise RuntimeError(
+                    "Couldn't find python libraries for Python %s from %s" % (
+                        python_major,
+                        lib_folders
+                    )
+                )
+            site_path = os.path.join(
+                base_path,
+                "lib",
+                folder,
+                "site-packages"
+            )
 
-        if pyside_root:
-            # Add PySide2 path to PYTHONPATH
-            if pyside_root not in sys.path:
-                sys.path.append(pyside_root)
+        os.environ["VIRTUAL_ENV"] = base_path
+        # Split PATH, prepend our bin_folder and join back using the os path
+        # separator (":" or ";")
+        os.environ["PATH"] = os.pathsep.join(
+            [os.path.join(base_path, bin_folder)] + os.environ.get("PATH", "").split(os.pathsep)
+        )
 
-            try:
-                self.log_debug("Attempting to import PySide2 from %s" % pyside_root)
-                from PySide2 import QtCore, QtGui, QtWidgets
-                import PySide2
-                
-                self.log_debug("Successfully initialized PySide2 '%s' located in %s." 
-                               % (PySide2.__version__, PySide2.__file__))
-            except ImportError as e:
-                self.log_warning("Error importing PySide2: %s" % e)        
-            except Exception as e:
-                self.log_warning("Error setting up PySide2. Pyside2-based UI support will not "
-                                 "be available: %s" % e)        
+        # Add the libraries to the host python import mechanism
+        prev_length = len(sys.path)
+        site.addsitedir(site_path)
+        sys.path[:] = sys.path[prev_length:] + sys.path[0:prev_length]
+
+        sys.real_prefix = sys.prefix
+        sys.prefix = base_path
 
     def destroy_framework(self):
         self.log_debug("%s: Destroying UnrealQtFramework..." % self)
+
+    @classmethod
+    def platform_name(cls):
+        """
+        Return a name for the current os that can be used
+        when building os specific paths
+
+        :returns: A name that can be used when building os specific paths.
+        :raises ValueError: if the current os is not supported.
+        """
+        platform_names = {"Darwin": "osx", "Linux": "linux", "Windows": "windows"}
+        pname = platform_names.get(platform.system())
+
+        if not pname:
+            raise ValueError("Platform %s is not supported" % platform.system())
+        return pname
+
+    @classmethod
+    def bin_folder(cls, vendor=None):
+        """
+        Returns the bin folder for the current os
+        and the given vendor where binaries can be found
+
+        :param str vendor: An optional vendor name that will be included in the path.
+        :returns: Full path to a folder.
+        """
+        pname = cls.platform_name()
+        if vendor:
+            return os.path.join(os.path.dirname(__file__), "vendors", vendor, "bin", pname)
+        return os.path.join(os.path.dirname(__file__), "vendors", "bin", pname)
